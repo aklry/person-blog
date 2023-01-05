ef23eea6070dbef24f9d5c79e22dceb5b032ca23
aklry <3265242862@qq.com>
2023-01-05 19:01:20 +0800

    前端项目构建

---
+++ blog-ui/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <el-button type="primary">primary</el-button>
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import { listUser } from "@/api/user.js";
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+  mounted() {
+    listUser().then((res) => {
+      console.log(res);
+    });
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
