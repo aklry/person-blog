9fece7fde9f769154bddb5a086d7dc1e27643717
aklry <3265242862@qq.com>
2023-01-10 19:48:56 +0800

    配置代理

--- blog-ui/vue.config.js
+++ blog-ui/vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    port: 80,
+    proxy: 'http://localhost:8088/'
+  }
 })
