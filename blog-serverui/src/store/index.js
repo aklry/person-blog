2c739f77931d4aca2e98217cefee2e1d2fd52bc8
aklry <3265242862@qq.com>
2023-02-09 00:27:41 +0800

    单独抽离vuex各个模块为一个文件

--- blog-serverui/src/store/index.js
+++ blog-serverui/src/store/index.js
@@ -1,17 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import state from './state'
+import actions from './actions'
+import mutations from './actions'
+import getters from './actions'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  state,
+  actions,
+  mutations,
+  getters
 })
