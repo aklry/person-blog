cc7f0d0eba22990861c99830282a79bce93dba3d
aklry <3265242862@qq.com>
2023-02-10 11:32:32 +0800

    引入公共样式

--- blog-serverui/src/main.js
+++ blog-serverui/src/main.js
@@ -6,6 +6,10 @@ import store from './store'
 // 引入element ui
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+
+//引入公共样式
+import '@/common/css/reset.css'
+
 Vue.config.productionTip = false
 Vue.use(ElementUI);
 new Vue({
