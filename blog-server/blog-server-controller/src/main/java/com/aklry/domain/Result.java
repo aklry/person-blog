759117238343ad1f2655f833191fdb5ad7220493
aklry <3265242862@qq.com>
2023-02-11 14:56:57 +0800

    为控制层实体类Result添加变量token

--- blog-server/blog-server-controller/src/main/java/com/aklry/domain/Result.java
+++ blog-server/blog-server-controller/src/main/java/com/aklry/domain/Result.java
@@ -6,13 +6,17 @@ package com.aklry.domain;
 public class Result {
     public boolean flag;//请求是否成功
     public String message;//请求提示信息
-
+    public String token; //用户登录时的token
     public Result() {
     }
 
     public Result(boolean flag, String message) {
         this.flag = flag;
         this.message = message;
+    }
+
+    public Result(String token) {
+        this.token = token;
     }
 
     public boolean isFlag() {
@@ -31,11 +35,20 @@ public class Result {
         this.message = message;
     }
 
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
     @Override
     public String toString() {
         return "Result{" +
                 "flag=" + flag +
                 ", message='" + message + '\'' +
+                ", token='" + token + '\'' +
                 '}';
     }
 }
