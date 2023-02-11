c694280df4f6079ca41179198fbfc9e7659a2b8c
aklry <3265242862@qq.com>
2023-02-11 10:55:32 +0800

    界面初始搭建

---
+++ blog-serverui/src/components/Layout/index.vue
@@ -0,0 +1,84 @@
+<template>
+    <div class="layout">
+        <el-container>
+            <el-aside width="200px">
+                <NavMenu />
+            </el-aside>
+            <el-main>
+                <el-header>
+                    <div class="user-info">
+                        <img class="avatar" src="@/assets/a1.png" alt="">
+                        <!-- <span class="username"></span> -->
+                        <el-dropdown>
+                            <span class="el-dropdown-link">
+                               <i class="el-icon-arrow-down el-icon--right"></i>
+                            </span>
+                            <el-dropdown-menu slot="dropdown">
+                                <el-dropdown-item>
+                                    <span @click="logout">登出</span>
+                                </el-dropdown-item>
+                            </el-dropdown-menu>
+                        </el-dropdown>
+                    </div>
+                </el-header>
+
+                <div class="content">
+                    <router-view></router-view>
+                </div>
+            </el-main>
+        </el-container>
+    </div>
+</template>
+
+<script>
+import NavMenu from '@/components/NavMenu.vue';
+export default {
+    data () {
+        return {
+
+        }
+    },
+    components: {
+        NavMenu
+    },
+    methods: {
+        logout () {
+            console.log('登出')
+        }
+    }
+}
+</script>
+
+<style scoped>
+.layout {
+    height: 100%;
+}
+
+.el-container {
+    height: 100%;
+}
+
+.el-main {
+    padding: 0;
+}
+
+.el-header {
+    box-shadow: 0 0 5px #ccc;
+}
+
+.content {
+    padding: 20px;
+}
+
+.user-info {
+  float: right;
+  margin-right: 20px;
+  margin-top: 10px;
+}
+.avatar {
+  width: 30px;
+  height: 30px;
+}
+</style>>
+
+</style>
---
+++ blog-serverui/src/components/NavMenu.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="nav-menu">
+        <el-menu default-active="2" class="el-menu-vertical-demo" router background-color="#001529" text-color="#999"
+            unique-opened active-text-color="#fff">
+            <div class="logo">
+                <img src="@/assets/logo.png" alt="" />
+                <span style="font-size: 16px;">个人博客管理后台</span>
+            </div>
+            <el-menu-item index="/home">
+                <i class="el-icon-s-home"></i>
+                <span slot="title">首页</span>
+            </el-menu-item>
+            <el-submenu index="menu">
+                <template slot="title">
+                    <i class="el-icon-setting"></i>
+                    <span slot="title">系统设置</span>
+                </template>
+                <el-menu-item index="/menu">
+                    <span slot="title">菜单管理</span>
+                </el-menu-item>
+                <el-menu-item index="/role">
+                    <span slot="title">角色管理</span>
+                </el-menu-item>
+                <el-menu-item index="/admin">
+                    <span slot="title">管理员管理</span>
+                </el-menu-item>
+                <el-menu-item index="/banner">
+                    <span slot="title">轮播图管理</span>
+                </el-menu-item>
+            </el-submenu>
+        </el-menu>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "NavBar"
+};
+</script>
+
+<style scoped>
+.logo {
+    height: 70px;
+    color: #fff;
+    font-size: 20px;
+    line-height: 70px;
+    text-align: center;
+}
+
+.logo img {
+    width: 30px;
+    height: 30px;
+    vertical-align: middle;
+    border-radius: 5px;
+}
+
+.nav-menu {
+    height: 100%;
+    background-color: #001529;
+}
+
+.el-menu {
+    width: 200px;
+}
+</style>
