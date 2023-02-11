8a6396e03d6d0c75191a70d1a8acb61b0473e9ee
aklry <3265242862@qq.com>
2023-02-11 16:36:32 +0800

    在业务层编写持久层实现类

--- blog-server/blog-server-service/src/main/java/com/aklry/service/AdminService.java
+++ blog-server/blog-server-service/src/main/java/com/aklry/service/AdminService.java
@@ -4,6 +4,7 @@ import com.aklry.dao.AdminDao;
 import com.aklry.domain.Admin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 @Service
@@ -17,5 +18,9 @@ public class AdminService {
 
     public Admin getAdminInfo(String username, String password) {
         return adminDao.findByParams(username, password);
+    }
+    @Transactional
+    public String addAdmin(Admin admin) {
+        return adminDao.addAdmin(admin);
     }
 }
