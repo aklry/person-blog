67bf414f02d68d775b65de59abd5df7230112594
aklry <3265242862@qq.com>
2023-02-13 21:33:02 +0800

    修改验证密码的正则表达式

--- blog-serverui/src/mixins/login.js
+++ blog-serverui/src/mixins/login.js
@@ -12,7 +12,7 @@ export default {
                 ],
                 password: [
                     { required: true, message: "请输入至少六位包含数字和字母的组合", trigger: "blur" },
-                    {pattern: '^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,16}$',message:'密码格式不正确',trigger:'change'}
+                    {pattern: '^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,16}$',message:'密码格式不正确',trigger:'blur'}
                 ]
             }
         }
