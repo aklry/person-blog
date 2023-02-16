3d9cc1574d478d62809f7dd43f9df8c30d207e20
Aklry <74409739+aklry@users.noreply.github.com>
2023-02-16 13:33:37 +0800

    完善登录的逻辑判断

--- blog-server/blog-server-controller/src/main/java/com/aklry/controller/AdminController.java
+++ blog-server/blog-server-controller/src/main/java/com/aklry/controller/AdminController.java
@@ -23,27 +23,32 @@ public class AdminController {
     }
     @PostMapping("/login")
     public List<Object> login(@RequestBody Admin admin) {
-        System.out.println(admin);
-        Admin adminInfo = adminService.getAdminInfo(admin);
         List<Object> endResult = new ArrayList<>();
         result = new Result();
-        if (adminInfo != null) {
-            String name = adminInfo.getUsername();
-            String adminPassword = adminInfo.getPassword();
-            if (name != null && adminPassword != null && name.length() != 0 && adminPassword.length() != 0) {
-                result.flag = true;
-                result.message = "登录成功";
-                result.token = Utils.getUUID();
-                endResult.add(result);
-                endResult.add(adminInfo);
-            }
-        } else {
-            result.flag = false;
-            result.message = "没有该用户";
-            result.token = null;
-            endResult.add(result);
-        }
-
+       if (admin.getUsername().length() != 0 || admin.getPassword().length() != 0) {
+           Admin adminInfo = adminService.getAdminInfo(admin);
+           if (adminInfo != null) {
+               String name = adminInfo.getUsername();
+               String adminPassword = adminInfo.getPassword();
+               if (name != null && adminPassword != null && name.length() != 0 && adminPassword.length() != 0) {
+                   result.flag = true;
+                   result.message = "登录成功";
+                   result.token = Utils.getUUID();
+                   endResult.add(result);
+                   endResult.add(adminInfo);
+               }
+           } else {
+               result.flag = false;
+               result.message = "没有该用户";
+               result.token = null;
+               endResult.add(result);
+           }
+       } else {
+           result.flag = false;
+           result.message = "请输入用户名或密码";
+           result.token = null;
+           endResult.add(result);
+       }
         return endResult;
     }
 
