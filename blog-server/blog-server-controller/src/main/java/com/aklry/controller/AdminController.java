37298da83448db16977be6d69084a0c8212b2246
Aklry <74409739+aklry@users.noreply.github.com>
2023-02-17 10:32:52 +0800

    编写添加管理员的方法

--- blog-server/blog-server-controller/src/main/java/com/aklry/controller/AdminController.java
+++ blog-server/blog-server-controller/src/main/java/com/aklry/controller/AdminController.java
@@ -1,11 +1,10 @@
 package com.aklry.controller;
 
 import com.aklry.domain.Admin;
+import com.aklry.domain.Result;
 import com.aklry.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,7 +13,7 @@ import java.util.List;
 public class AdminController {
     @Autowired
     private AdminService adminService;
-
+    private Result result;
     /**
      * 获取所有管理员
      * @return
@@ -22,5 +21,25 @@ public class AdminController {
     @GetMapping ("/listAllAdmin")
     public List<Admin> listAllAdmin() {
         return adminService.listAdmin();
+    }
+
+    @PostMapping("/add")
+    public Result addAdmin(@RequestBody Admin admin) {
+        if (admin.getUsername().length() != 0 && admin.getPassword().length() != 0) {
+            Admin adminInfo = adminService.getAdminInfo(admin);
+            result = new Result();
+            if (adminInfo == null) {
+                adminService.addAdmin(admin.getUsername(),admin.getPassword());
+                result.flag = true;
+                result.message = "添加成功";
+            } else {
+                result.message = "该用户已存在";
+                result.flag = false;
+            }
+        } else {
+            result.message = "请输入用户名或密码";
+            result.flag = false;
+        }
+        return result;
     }
 }
