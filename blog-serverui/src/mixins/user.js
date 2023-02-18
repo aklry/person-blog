d252b394c20abf1506afadb2055999ef5c13f90d
Aklry <74409739+aklry@users.noreply.github.com>
2023-02-18 21:46:50 +0800

    编写用户管理的编辑功能

--- blog-serverui/src/mixins/user.js
+++ blog-serverui/src/mixins/user.js
@@ -3,7 +3,9 @@ export default {
         return {
             userList: [],
             search: "",
-            formLabelWidth: '60px',
+            formLabelWidth: '120px',
+            isVisible: false,
+            id: 0,
             form: {
                 name: "",
                 password: "",
@@ -20,8 +22,36 @@ export default {
                 password: [
                     { required: true, message: "请输入至少六位包含数字和字母的组合", trigger: "blur" },
                     { pattern: '^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,16}$', message: '密码格式不正确', trigger: 'blur' }
+                ],
+                phoneNumber: [
+                    { required: true, message: "请输入11位电话号码", trigger: "blur" },
+                    {pattern: '0?(13|14|15|17|18)[0-9]{9}',message: '请输入正确的电话号码',trigger: 'blur'}
+                ],
+                address: [
+                    { required: true, message: "请输入你的地址", trigger: "blur" },
+                ],
+                sex: [
+                    { required: true, message: "请输入你的性别", trigger: "blur" },
+                    {pattern: '^(男|女){1}$',message: '请输入性别格式(男或女)',trigger: 'blur'}
                 ]
             },
+        }
+    },
+    methods: {
+        changeIsVisible(val) {
+            this.isVisible = val
+        },
+        handleEdit(index, row) {
+            this.isVisible = true
+        },
+        handleDelete(index, row) {
+            console.log(row)
+        },
+        cancel() {
+            this.isVisible = false
+        },
+        submit() {
+            console.log('submit')
         }
     }
 }
--- blog-serverui/src/views/user/index.vue
+++ blog-serverui/src/views/user/index.vue
@@ -17,6 +17,33 @@
         </template>
         <template slot-scope="scope">
           <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">Edit</el-button>
+          <EditDialog :isVisible="isVisible" @changeIsVisible="changeIsVisible" :id="id">
+            <template #input>
+              <el-form :model="form" :rules="rules">
+                <el-form-item label="用户名" :label-width="formLabelWidth" prop="username">
+                  <el-input v-model="form.username" autocomplete="off" ref="username"></el-input>
+                </el-form-item>
+                <el-form-item label="性别" :label-width="formLabelWidth" prop="sex">
+                  <el-input v-model="form.sex" autocomplete="off" ref="sex"></el-input>
+                </el-form-item>
+                <el-form-item label="密码" prop="password" :label-width="formLabelWidth">
+                  <el-input v-model="form.password" show-password ref="password" autocomplete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="电话号码" prop="phoneNumber" :label-width="formLabelWidth">
+                  <el-input v-model="form.phoneNumber" ref="phoneNumber" autocomplete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="地址" prop="address" :label-width="formLabelWidth">
+                  <el-input v-model="form.address" ref="address" autocomplete="off"></el-input>
+                </el-form-item>
+              </el-form>
+            </template>
+            <template #button>
+              <div slot="footer" class="dialog-footer">
+                <el-button @click="cancel">取 消</el-button>
+                <el-button type="primary" @click="submit">修 改</el-button>
+              </div>
+            </template>
+          </EditDialog>
           <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">Delete</el-button>
         </template>
       </el-table-column>
@@ -28,10 +55,12 @@
 import Breadcrumb from '@/components/Breadcrumb'
 import user from '@/mixins/user'
 import api from '@/api/user'
+import EditDialog from '@/components/Dialog/EditDialog.vue'
 export default {
   mixins: [user],
   components: {
-    Breadcrumb
+    Breadcrumb,
+    EditDialog
   },
   mounted() {
     api.getAllUsers().then(res => {
