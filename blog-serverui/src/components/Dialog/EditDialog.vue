27ffc5a3d1680bb260b57d34d66d19ee4d2895d0
Aklry <74409739+aklry@users.noreply.github.com>
2023-02-18 21:23:46 +0800

    使用插槽优化编辑dialog的代码结构

--- blog-serverui/src/components/Dialog/EditDialog.vue
+++ blog-serverui/src/components/Dialog/EditDialog.vue
@@ -1,28 +1,12 @@
 <template>
     <el-dialog :visible="isVisible">
-        <el-form :model="form" :rules="rules">
-            <el-form-item label="用户名" :label-width="formLabelWidth" prop="username">
-                <el-input v-model="form.username" autocomplete="off" ref="username"></el-input>
-            </el-form-item>
-            <el-form-item label="密码" prop="password" :label-width="formLabelWidth">
-                <el-input v-model="form.password" show-password ref="password" autocomplete="off"></el-input>
-            </el-form-item>
-        </el-form>
-        <div slot="footer" class="dialog-footer">
-            <el-button @click="cancel">取 消</el-button>
-            <el-button type="primary" @click="submit">修 改</el-button>
-        </div>
+        <slot name="input" />
+        <slot name="button" />
     </el-dialog>
 </template>
 
 <script>
-import mixin from '@/mixins/editDialog'
-import api from '@/api';
-
-import utils from '@/util/utils';
 export default {
-    inject: ['reload'],
-    mixins: [mixin],
     props: {
         isVisible: {
             type: Boolean,
@@ -32,29 +16,6 @@ export default {
         },
         id: {
             type: Number
-        }
-    },
-    methods: {
-        cancel() {
-            this.$emit("changeIsVisible", false)
-        },
-        submit() {
-            this.$emit("changeIsVisible", false)
-            const username = this.$data.form.username
-            const password = this.$data.form.password
-            const id = this.id
-            api.update({
-                id,
-                username,
-                password
-            }).then(res => {
-                if (res.data.flag) {
-                    utils.alert(this, res.data.message)
-                    this.reload()
-                } else {
-                    utils.alert(this, res.data.message)
-                }
-            })
         }
     }
 }
--- blog-serverui/src/views/admin/index.vue
+++ blog-serverui/src/views/admin/index.vue
@@ -3,43 +3,42 @@
     <Breadcrumb>
       <template>管理员管理</template>
     </Breadcrumb>
-    <el-table
-      :data="
-        tableData.filter((data) => !search || data.username.includes(search))
-      "
-      style="width: 100%"
-    >
+    <el-table :data="
+      tableData.filter((data) => !search || data.username.includes(search))
+    " style="width: 100%">
       <el-table-column label="id" prop="id"> </el-table-column>
       <el-table-column label="用户名" prop="username"> </el-table-column>
       <el-table-column label="密码" prop="password"> </el-table-column>
       <el-table-column align="right">
         <template slot="header" slot-scope="scope">
           <div class="container">
-            <el-input
-              v-model="search"
-              size="mini"
-              placeholder="输入关键字搜索"
-            />
+            <el-input v-model="search" size="mini" placeholder="输入关键字搜索" />
             <el-button size="mini" @click="isShowHandle()">Add</el-button>
           </div>
           <!-- 弹出框 -->
           <Dialog :isShow="isShow" @changeIsShow="changeIsShow" />
         </template>
         <template slot-scope="scope">
-          <el-button size="mini" @click="handleEdit(scope.$index, scope.row)"
-            >Edit</el-button
-          >
-          <EditDialog
-            :isVisible="isVisible"
-            @changeIsVisible="changeIsVisible"
-            :id="id"
-          />
-          <el-button
-            size="mini"
-            type="danger"
-            @click="handleDelete(scope.$index, scope.row)"
-            >Delete</el-button
-          >
+          <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">Edit</el-button>
+          <EditDialog :isVisible="isVisible" @changeIsVisible="changeIsVisible" :id="id">
+            <template #input>
+              <el-form :model="form" :rules="rules">
+                <el-form-item label="用户名" :label-width="formLabelWidth" prop="username">
+                  <el-input v-model="form.username" autocomplete="off" ref="username"></el-input>
+                </el-form-item>
+                <el-form-item label="密码" prop="password" :label-width="formLabelWidth">
+                  <el-input v-model="form.password" show-password ref="password" autocomplete="off"></el-input>
+                </el-form-item>
+              </el-form>
+            </template>
+            <template #button>
+              <div slot="footer" class="dialog-footer">
+                <el-button @click="cancel">取 消</el-button>
+                <el-button type="primary" @click="submit">修 改</el-button>
+              </div>
+            </template>
+          </EditDialog>
+          <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">Delete</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -47,26 +46,26 @@
 </template>
   
 <script>
-import api from "@/api";
-import mixin from "@/mixins/table";
+import api from "@/api"
+import table from "@/mixins/table"
+import editDialog from '@/mixins/editDialog'
 import Dialog from "@/components/Dialog";
-import EditDialog from "@/components/Dialog/EditDialog.vue";
-import utils from "@/util/utils";
-import Breadcrumb from "@/components/Breadcrumb";
+import EditDialog from "@/components/Dialog/EditDialog.vue"
+import utils from "@/util/utils"
+import Breadcrumb from "@/components/Breadcrumb"
 export default {
-  mixins: [mixin],
+  mixins: [table, editDialog],
   inject: ["reload"],
   methods: {
     handleEdit(index, row) {
-      console.log(index, row);
-      this.$data.isVisible = true;
-      this.$data.id = row.id;
+      this.$data.isVisible = true
+      this.$data.id = row.id
     },
     handleDelete(index, row) {
       this.$confirm("此操作将永久删除该管理员, 是否继续?", "提示", {
         confirmButtonText: "确定",
         cancelButtonText: "取消",
-        type: "warning",
+        type: "warning"
       })
         .then(() => {
           api.delete(row.id).then((res) => {
@@ -74,8 +73,29 @@ export default {
             this.reload();
           });
         })
-        .catch(() => {});
+        .catch(() => { });
     },
+    cancel() {
+      this.isVisible = false
+    },
+    submit() {
+      this.isVisible = false
+      const username = this.$data.form.username
+      const password = this.$data.form.password
+      const id = this.id
+      api.update({
+        id,
+        username,
+        password
+      }).then(res => {
+        if (res.data.flag) {
+          utils.alert(this, res.data.message)
+          this.reload()
+        } else {
+          utils.alert(this, res.data.message)
+        }
+      })
+    }
   },
   components: {
     Dialog,
