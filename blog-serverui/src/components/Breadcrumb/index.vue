bcedc13f59657d38b7d0df65cb0be7aee7a4cc2a
Aklry <74409739+aklry@users.noreply.github.com>
2023-02-18 17:00:43 +0800

    封装一个面包屑导航

---
+++ blog-serverui/src/components/Breadcrumb/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <el-breadcrumb separator="/">
+        <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+        <el-breadcrumb-item>
+            <a href="/">
+                <slot></slot>
+            </a>
+        </el-breadcrumb-item>
+  </el-breadcrumb>
+</template>
