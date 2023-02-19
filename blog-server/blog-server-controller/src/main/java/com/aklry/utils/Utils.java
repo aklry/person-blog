4ce586c23c2e177e7897b7f72945b13a45f2f75e
Aklry <74409739+aklry@users.noreply.github.com>
2023-02-19 19:16:09 +0800

    编写返回结果对象的工厂类

--- blog-server/blog-server-controller/src/main/java/com/aklry/utils/Utils.java
+++ blog-server/blog-server-controller/src/main/java/com/aklry/utils/Utils.java
@@ -1,9 +1,15 @@
 package com.aklry.utils;
 
+import com.aklry.domain.Result;
+
 import java.util.UUID;
 
 public class Utils {
     public static String getUUID() {
         return UUID.randomUUID().toString();
+    }
+
+    public static Result getResult() {
+        return new Result();
     }
 }
