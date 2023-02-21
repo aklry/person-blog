627e76c595d70e186de6f09247a600acc9950174
Aklry <74409739+aklry@users.noreply.github.com>
2023-02-21 21:02:23 +0800

    删除blog-ui

--- blog-ui/src/api/test.js
+++
@@ -1,9 +0,0 @@
-import request from "@/api";
-
-// 查询用户列表
-export function listUser() {
-    return request({
-        url: '/listUser',
-        method: 'get'
-    })
-}
--- blog-ui/vue.config.js
+++
@@ -1,8 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  devServer: {
-    port: 80,
-    proxy: 'http://localhost:8088/'
-  }
-})
