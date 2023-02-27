19d0a6cb1a69ded9e16e3618739f2cebbf036aa6
Aklry <74409739+aklry@users.noreply.github.com>
2023-02-27 22:07:02 +0800

    feat: 为所有页面添加局部刷新功能

--- blog-ui/src/App.vue
+++ blog-ui/src/App.vue
@@ -1,8 +1,29 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view v-if="isRouterAlive" />
   </div>
 </template>
+<script>
+
+export default {
+  data() {
+    return {
+      isRouterAlive: true
+    }
+  },
+  methods: {
+    reload() {
+      this.isRouterAlive = false
+      this.$nextTick(() => this.isRouterAlive = true)
+    }
+  },
+  provide() {
+    return {
+      reload: this.reload
+    }
+  }
+}
+</script>
 
 <style>
 #app {
