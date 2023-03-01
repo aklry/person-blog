2e4c0e324a6b7daf0052db35adbabf8f33d6092b
Aklry <74409739+aklry@users.noreply.github.com>
2023-03-01 19:42:51 +0800

    refactor: 注册vue-ele-form以及image-loader插件并进行全局配置

--- blog-ui/src/main.js
+++ blog-ui/src/main.js
@@ -6,12 +6,30 @@ import store from './store'
 // 引入element ui
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+// 引入 vue-ele-form
+import EleForm from 'vue-ele-form'
+import EleFormImageUploader from 'vue-ele-form-image-uploader'
+//引入swiper插件
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'swiper/css/swiper.css'
 //引入公共样式
 import '@/common/css/reset.css'
 Vue.config.productionTip = false
+//注册element-ui
 Vue.use(ElementUI)
+// 注册 vue-ele-form
+Vue.use(EleForm, {
+   // number类型
+  number: {
+    min: 0 // 所有 number 类型, 最小值为 0
+  },
+  // 对所有具有上传属性的组件适用
+  upload: {
+    fileSize: 10
+  }
+})
+// 注册 image-uploader 组件
+Vue.component('image-uploader', EleFormImageUploader)
 Vue.use(VueAwesomeSwiper)
 new Vue({
   router,
