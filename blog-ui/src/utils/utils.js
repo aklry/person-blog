f4e83eb0604259504427e694959d43eb3b49b57e
Aklry <74409739+aklry@users.noreply.github.com>
2023-03-01 17:24:55 +0800

    refactor: 封装转换localStorage数据格式的方法

--- blog-ui/src/utils/utils.js
+++ blog-ui/src/utils/utils.js
@@ -4,5 +4,8 @@ export default {
         vm.$alert(info, '提示', {
             confirmButtonText: '确定'
         })
+    },
+    dataTransfer(localStorage, key) {
+        return JSON.parse(localStorage.getItem(key))
     }
 }
