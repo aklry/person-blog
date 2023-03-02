4fe18d9a1fa72210d40934959e4870a218bfba9a
Aklry <74409739+aklry@users.noreply.github.com>
2023-03-02 22:36:36 +0800

    refactor: 添加获取所有博客的接口

--- blog-ui/src/api/blog.js
+++ blog-ui/src/api/blog.js
@@ -1,13 +1,17 @@
 import axios from "../utils/request"
 
 const base = {
-    addBlog: '/publishBlog'
+    addBlog: '/publishBlog',
+    listAllBlog: '/listAllBlog'
 }
 
 const blogApi = {
     publishBlog(params) {
         console.log(params)
         return axios.post(base.addBlog, params)
+    },
+    listAllBlog() {
+        return axios.post(base.listAllBlog)
     }
 }
 
