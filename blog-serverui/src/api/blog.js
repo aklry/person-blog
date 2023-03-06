c6536328739693b4e813711d41c587d32c612e6d
Aklry <74409739+aklry@users.noreply.github.com>
2023-03-06 21:24:20 +0800

    refactor: 编写调用后端接口的方法

--- blog-serverui/src/api/blog.js
+++ blog-serverui/src/api/blog.js
@@ -1,12 +1,16 @@
 import axios from '../util/request'
 
 const base = {
-    listAllBlog: 'listAllBlog'
+    listAllBlog: '/listAllBlog',
+    deleteBlogById: '/deleteBlogById'
 }
 
 const blogApi = {
     listAllBlog(params) {
         return axios.post(base.listAllBlog, params)
+    },
+    deleteBlogById(params) {
+        return axios.post(base.deleteBlogById, params)
     }
 }
 
