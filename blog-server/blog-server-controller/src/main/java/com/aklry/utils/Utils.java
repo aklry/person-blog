851370073af15aa919ea97d532c4eb5b1821546e
Aklry <74409739+aklry@users.noreply.github.com>
2023-03-06 19:08:49 +0800

    添加一个工具类处理前端富文本编辑器有html标签的问题

--- blog-server/blog-server-controller/src/main/java/com/aklry/utils/Utils.java
+++ blog-server/blog-server-controller/src/main/java/com/aklry/utils/Utils.java
@@ -4,6 +4,8 @@ import com.aklry.domain.Result;
 
 import java.util.UUID;
 
+import java.util.regex.*;
+
 public class Utils {
     public static String getUUID() {
         return UUID.randomUUID().toString();
@@ -16,4 +18,46 @@ public class Utils {
     public static Result getResult() {
         return new Result();
     }
+    /**
+     * 去除富文本编辑器标签
+     *
+     * @param inputString
+     * @return
+     */
+    public static String removeHtmlTag(String inputString) {
+        if (inputString == null)
+            return null;
+        String htmlStr = inputString; // 含html标签的字符串
+        String textStr = "";
+        Pattern p_script;
+        Matcher m_script;
+        Pattern p_style;
+        Matcher m_style;
+        Pattern p_html;
+        Matcher m_html;
+        try {
+            //定义script的正则表达式{或<script[^>]*?>[\\s\\S]*?<\\/script>
+            String regEx_script = "<[\\s]*?script[^>]*?>[\\s\\S]*?<[\\s]*?\\/[\\s]*?script[\\s]*?>";
+            //定义style的正则表达式{或<style[^>]*?>[\\s\\S]*?<\\/style>
+            String regEx_style = "<[\\s]*?style[^>]*?>[\\s\\S]*?<[\\s]*?\\/[\\s]*?style[\\s]*?>";
+            String regEx_html = "<[^>]+>"; // 定义HTML标签的正则表达式
+            p_script = Pattern.compile(regEx_script, Pattern.CASE_INSENSITIVE);
+            m_script = p_script.matcher(htmlStr);
+            htmlStr = m_script.replaceAll(""); // 过滤script标签
+            p_style = Pattern.compile(regEx_style, Pattern.CASE_INSENSITIVE);
+            m_style = p_style.matcher(htmlStr);
+            htmlStr = m_style.replaceAll(""); // 过滤style标签
+            p_html = Pattern.compile(regEx_html, Pattern.CASE_INSENSITIVE);
+            m_html = p_html.matcher(htmlStr);
+            htmlStr = m_html.replaceAll(""); // 过滤html标签
+            textStr = htmlStr;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return textStr;// 返回文本字符串
+    }
+
+
+
+
 }
