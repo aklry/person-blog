cc401bdfd54f29857dabe7680abdd1d027744b3c
Aklry <74409739+aklry@users.noreply.github.com>
2023-03-13 20:01:28 +0800

    修改实体类，添加user_id字段

--- blog-server/blog-server-dao/src/main/java/com/aklry/domain/Comment.java
+++ blog-server/blog-server-dao/src/main/java/com/aklry/domain/Comment.java
@@ -9,14 +9,16 @@ public class Comment {
     private int id;
     private Date dateTime;
     private String content;
+    private int user_id;
 
     public Comment() {
     }
 
-    public Comment(int id, Date dateTime, String content) {
+    public Comment(int id, Date dateTime, String content, int user_id) {
         this.id = id;
         this.dateTime = dateTime;
         this.content = content;
+        this.user_id = user_id;
     }
 
     public int getId() {
@@ -43,12 +45,21 @@ public class Comment {
         this.content = content;
     }
 
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
     @Override
     public String toString() {
         return "Comment{" +
                 "id=" + id +
                 ", dateTime=" + dateTime +
                 ", content='" + content + '\'' +
+                ", user_id=" + user_id +
                 '}';
     }
 }
