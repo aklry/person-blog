e86442762bcd1848f03745ecd1ca4b1a4a43453a
Aklry <74409739+aklry@users.noreply.github.com>
2023-03-17 23:13:27 +0800

    feat: 实现发表评论的功能

--- blog-ui/src/mixins/blog.js
+++ blog-ui/src/mixins/blog.js
@@ -1,9 +1,16 @@
 import { mapMutations } from "vuex"
+import commentApi from "@/api/comment"
+import moment from "moment"
 export default {
     data() {
         return {
             formData: {
                 content: ''
+            },
+            rules: {
+                content: [
+                    {required: true, message: "请输入你的评论", trigger: "blur"}
+                ]
             }
         }
     },
@@ -12,6 +19,28 @@ export default {
         logout() {
             this.handleLogout()
             this.$router.push({name: 'Login'})
+        },
+        comment(userInfo, formName) {
+            this.$refs[formName].validate((valid) => {
+                if (valid) {
+                    const dateTime = moment().format('YYYY-MM-DD hh:mm:ss')
+                    commentApi.addComment({
+                        content: this.formData.content,
+                        dateTime,
+                        user_id: userInfo.id
+                    }).then(res => {
+                        if (res.data.flag) {
+                            this.$message.success(res.data.message)
+                            this.$refs[formName].resetFields()
+                        } else {
+                            this.$message.success(res.data.message)
+                            this.$refs[formName].resetFields()
+                        }
+                    })
+                } else {
+                    return false
+                }
+            })
         }
     }
 }
