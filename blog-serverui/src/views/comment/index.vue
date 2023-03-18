5d64c61d8124e49043db976b1796341b5596c323
Aklry <74409739+aklry@users.noreply.github.com>
2023-03-18 17:39:10 +0800

    feat: 编写评论管理页面

--- blog-serverui/src/views/comment/index.vue
+++ blog-serverui/src/views/comment/index.vue
@@ -1,16 +1,34 @@
 <template>
-  <Breadcrumb>评论管理</Breadcrumb>
+  <div>
+    <Breadcrumb>评论管理</Breadcrumb>
+    <el-table :data="
+      commentList.filter((data) => !search || data.user.name.includes(search))
+    " style="width: 100%">
+      <el-table-column label="id" prop="id"> </el-table-column>
+      <el-table-column label="发表时间" prop="dateTime"> </el-table-column>
+      <el-table-column label="内容" prop="content"> </el-table-column>
+      <el-table-column label="发表人" prop="user.name"> </el-table-column>
+      <el-table-column align="right">
+        <template slot="header" slot-scope="scope">
+          <el-input v-model="search" size="mini" placeholder="输入关键字搜索" />
+        </template>
+        <template slot-scope="scope">
+          <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">Delete</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
 
 <script>
 import Breadcrumb from '@/components/Breadcrumb'
+import commentMixins from '@/mixins/comment'
 export default {
-    components: {
-        Breadcrumb
-    }
+  components: {
+    Breadcrumb
+  },
+  mixins: [commentMixins]
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
