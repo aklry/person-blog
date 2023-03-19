1f7ab6fc5f2c2275351b8acff37447a302b09367
Aklry <74409739+aklry@users.noreply.github.com>
2023-03-19 15:32:51 +0800

    编写查询博客及其相关评论的方法(一对多关联查询)

--- blog-server/blog-server-dao/src/main/java/com/aklry/dao/BlogDao.java
+++ blog-server/blog-server-dao/src/main/java/com/aklry/dao/BlogDao.java
@@ -2,6 +2,7 @@ package com.aklry.dao;
 
 import com.aklry.domain.Blog;
 import org.apache.ibatis.annotations.*;
+import org.apache.ibatis.mapping.FetchType;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,6 +12,7 @@ public interface BlogDao {
 
     /**
      * 添加一条博客
+     *
      * @param blog
      */
     @Insert("insert into blog(id, publishTime, title, author, content, category, type) values(default, #{publishTime}, #{title}, #{author}, #{content}, #{category}, #{type})")
@@ -18,6 +20,7 @@ public interface BlogDao {
 
     /**
      * 查询所有博客
+     *
      * @return
      */
     @Select("select * from blog")
@@ -25,6 +28,7 @@ public interface BlogDao {
 
     /**
      * 根据id查询博客内容
+     *
      * @param id
      * @return
      */
@@ -34,6 +38,7 @@ public interface BlogDao {
 
     /**
      * 根据id删除博客
+     *
      * @param id
      */
 
@@ -42,6 +47,7 @@ public interface BlogDao {
 
     /**
      * 根据id更新博客内容以及发表时间
+     *
      * @param blog
      */
 
@@ -49,10 +55,23 @@ public interface BlogDao {
     void updateContentById(Blog blog);
 
     /**
-     * 根据id查询博客
+     * 根据id查询博客及其相关评论
+     *
      * @param id
      * @return
      */
     @Select("select * from blog where id = #{id}")
+    @Results(id = "BlogDao", value = {
+            @Result(id = true, property = "id", column = "id"),
+            @Result(property = "publishTime", column = "publishTime"),
+            @Result(property = "title", column = "title"),
+            @Result(property = "author", column = "author"),
+            @Result(property = "content", column = "content"),
+            @Result(property = "category", column = "category"),
+            @Result(property = "type", column = "type"),
+            @Result(property = "comments", column = "id",
+                    many = @Many(select = "com.aklry.dao.CommentDao.findCommentByBlogId", fetchType = FetchType.LAZY)
+            )
+    })
     Blog findBlogById(@Param("id") int id);
 }
