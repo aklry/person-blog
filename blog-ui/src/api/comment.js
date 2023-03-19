1e303a17cabc292dffca1a0594b88013d27695dd
Aklry <74409739+aklry@users.noreply.github.com>
2023-03-19 15:43:31 +0800

    refactor: 使用axios调用后端查询评论及其用户的方法

--- blog-ui/src/api/comment.js
+++ blog-ui/src/api/comment.js
@@ -1,12 +1,16 @@
 import axios from '../utils/request'
 
 const base = {
-    addComment: '/addComment'
+    addComment: '/addComment',
+    listUsersComment: '/getAllComment'
 }
 
 const commentApi = {
     addComment(params) {
         return axios.post(base.addComment, params)
+    },
+    listUsersComment(params) {
+        return axios.post(base.listUsersComment, params)
     }
 }
 
