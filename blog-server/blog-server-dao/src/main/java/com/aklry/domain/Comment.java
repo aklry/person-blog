927c1d0d3bfdb5cd4023d19237340c104bcdb8e5
Aklry <74409739+aklry@users.noreply.github.com>
2023-03-19 17:32:56 +0800

    解决实体类中 发现有字段为null，在转化成json的时候，fasterxml.jackson将对象转换为json报错问题

--- blog-server/blog-server-dao/src/main/java/com/aklry/domain/Comment.java
+++ blog-server/blog-server-dao/src/main/java/com/aklry/domain/Comment.java
@@ -1,11 +1,15 @@
 package com.aklry.domain;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.util.Date;
 
 /**
  * 评论实体
  */
+//解决实体类中 发现有字段为null，在转化成json的时候，fasterxml.jackson将对象转换为json报错问题
+@JsonIgnoreProperties(value = { "hibernateLazyInitializer", "handler" })
 public class Comment {
     private int id;
     private Date dateTime;
