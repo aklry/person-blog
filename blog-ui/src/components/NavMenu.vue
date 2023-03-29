ea739c67066221a2f63ecad49d3b2a40aa8ac458
Aklry <74409739+aklry@users.noreply.github.com>
2023-03-29 15:47:48 +0800

    refactor: 取消使用插槽自定义导航栏内容，实现导航栏内容的一致性

--- blog-ui/src/components/NavMenu.vue
+++ blog-ui/src/components/NavMenu.vue
@@ -1,8 +1,17 @@
 <template>
     <div class="menu">
-        <el-menu class="el-menu-demo" :default-active="index" mode="horizontal" router background-color="#545c64" text-color="#fff"
-            active-text-color="#ffd04b">
-            <slot name="header" />
+        <el-menu class="el-menu-demo" :default-active="index" mode="horizontal" router background-color="#545c64"
+            text-color="#fff" active-text-color="#ffd04b">
+            <el-menu-item index="/">首页</el-menu-item>
+            <el-menu-item>
+                <router-link class="content" :to="{ name: 'Content', params: { content: 'vue' } }">vue</router-link>
+            </el-menu-item>
+            <el-menu-item>
+                <router-link class="content"
+                    :to="{ name: 'Content', params: { content: 'JavaScript' } }">JavaScript</router-link>
+            </el-menu-item>
+            <el-menu-item index="/user">个人中心</el-menu-item>
+            <el-menu-item index="/publishBlog">发表博客</el-menu-item>
         </el-menu>
     </div>
 </template>
