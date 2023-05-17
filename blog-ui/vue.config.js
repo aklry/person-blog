35ef78a779bdf9bf06489213cdbe21ba7a5220c5
Aklry <74409739+aklry@users.noreply.github.com>
2023-05-17 17:24:46 +0800

    ci(vue.config.js): 更改反向代理的代码书写方式

--- blog-ui/vue.config.js
+++ blog-ui/vue.config.js
@@ -3,10 +3,21 @@ const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
 module.exports = defineConfig({
   transpileDependencies: true,
   configureWebpack: {
-      plugins: [new NodePolyfillPlugin()]
+    plugins: [new NodePolyfillPlugin()]
   },
   devServer: {
     port: 80,
-    proxy: 'http://localhost:8088'
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8088',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      },
+      '/upload': {
+        target: 'http://localhost:8088'
+      }
+    }
   }
 })
