585ec7ed501117b1b9826643f10a5c415e4f4836
Aklry <74409739+aklry@users.noreply.github.com>
2023-05-22 21:20:27 +0800

    refactor: 修改路由

--- blog-serverui/src/router/index.js
+++ blog-serverui/src/router/index.js
@@ -20,7 +20,8 @@ const routes = [
         name: 'Home',
         meta: {
           isLogin: true
-        }
+        },
+        component: () => import('@/views/home')
       },
       {
         path: '/admin',
@@ -74,7 +75,7 @@ const routes = [
     name: 'Register',
     component: () => import('@/views/Register')
   }
-  
+
 ]
 
 const router = new VueRouter({
@@ -82,17 +83,18 @@ const router = new VueRouter({
 })
 
 router.beforeEach((to, from, next) => {
-    if (to.meta.isLogin) {
-      if (store.state.token) {
-      next()
-    } else {
-      next({
-        path: '/login'
-      })
-    }
-  } else {
-    next()
+  // 1.如果访问的是注册或者登录, 那么就放行
+  if (to.path === '/login' || to.path === '/') {
+    return next();
   }
+  // 2.获取当前的登录状态
+  const token = localStorage.getItem('token');
+  // 3.如果访问的是其它路由地址, 那么就需要判断是否已经登录
+  //   如果已经登录, 那么就放行, 如果没有登录, 那么就强制跳转到登录界面
+  if (!token) {
+    return next('/login');
+  }
+  next();
 })
 
 export default router
