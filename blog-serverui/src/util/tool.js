61407477b68b24fce34052c9b4b7550108c77024
Aklry <74409739+aklry@users.noreply.github.com>
2023-05-22 22:34:24 +0800

    refactor: 添加数据持久化存储函数

--- blog-serverui/src/util/tool.js
+++ blog-serverui/src/util/tool.js
@@ -1,3 +1,15 @@
+import store from "@/store"
+//数据持久化存储函数
+const dataStorage = function () {
+    const token = localStorage.getItem('token')
+    const adminInfo = JSON.parse(localStorage.getItem('adminInfo'))
+    if (token) {
+        store.commit('setAdminInfo', {
+            token,
+            adminInfo
+        })
+    }
+}
 // 封装兼容性方法getStyle(获取元素的css属性)
 const getStyle = function (elem, prop) {
     if (window.getComputedStyle) {
@@ -8,5 +20,6 @@ const getStyle = function (elem, prop) {
 }
 
 export default {
-    getStyle
+    getStyle,
+    dataStorage
 }
