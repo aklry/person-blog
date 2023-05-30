e21ee9cf7bdf7003ff62167b498657cfbaeddf96
Aklry <74409739+aklry@users.noreply.github.com>
2023-05-30 17:20:35 +0800

    chore: 更改代理的配置方式

--- blog-serverui/vue.config.js
+++ blog-serverui/vue.config.js
@@ -3,9 +3,20 @@ const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
 module.exports = defineConfig({
   transpileDependencies: true,
   configureWebpack: {
-      plugins: [new NodePolyfillPlugin()]
+    plugins: [new NodePolyfillPlugin()]
   },
   devServer: {
-    proxy: 'http://localhost:8088'
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8088',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      },
+      '/upload': {
+        target: 'http://localhost:8088'
+      }
+    }
   }
 })
