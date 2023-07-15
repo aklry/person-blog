1ba332a2757fbf73d3d95ac8a1ca07259197498e
Aklry <74409739+aklry@users.noreply.github.com>
2023-07-15 23:46:13 +0800

    实现分类管理的增删改查等逻辑

---
+++ blog-server/blog-server-controller/src/main/java/com/aklry/controller/CategoryController.java
@@ -0,0 +1,48 @@
+package com.aklry.controller;
+
+import com.aklry.domain.Category;
+import com.aklry.domain.Result;
+import com.aklry.service.CategoryService;
+import com.aklry.utils.Utils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/blog")
+public class CategoryController {
+    @Autowired
+    private CategoryService categoryService;
+    private Result result = Utils.getResult();
+    @GetMapping("/addCategory")
+    public Result addCategory(String category) {
+        if (!"".equals(category)) {
+            categoryService.addCategory(category);
+            result.message = "添加类目成功";
+            result.flag = true;
+        }
+        return result;
+    }
+
+    @GetMapping("/getAllCategory")
+    public List<Category> getAllCategory() {
+        return categoryService.getAllCategory();
+    }
+
+    @GetMapping("deleteCategory")
+    public Result deleteCategotyById(int id) {
+        //判断前端传递过来的参数是否正确
+        boolean flag = categoryService.deleteCategoryById(id);
+        if (flag) {
+            result.message = "删除成功";
+            result.flag = true;
+        } else {
+            result.message = "删除失败";
+            result.flag = false;
+        }
+        return result;
+    }
+}
---
+++ blog-server/blog-server-dao/src/main/java/com/aklry/dao/CategoryDao.java
@@ -0,0 +1,21 @@
+package com.aklry.dao;
+
+import com.aklry.domain.Category;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CategoryDao {
+    @Insert("insert into category(id, category) values(default, #{category})")
+    void addCategory(String category);
+
+    @Select("select * from category")
+    List<Category> getAllCategory();
+
+    @Delete("delete from category where id = #{id}")
+    void deleteCategoryById(int id);
+}
