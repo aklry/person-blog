fad85b595b556eb8d072d412d1b665e846257333
Aklry <74409739+aklry@users.noreply.github.com>
2023-07-15 18:46:24 +0800

    refactor: 修改接口传递的参数

--- blog-serverui/src/api/banner.js
+++ blog-serverui/src/api/banner.js
@@ -2,11 +2,27 @@ import { url } from './base'
 import request from '../util/request'
 
 const base = {
-    addBanner: '/addBanner'
+    addOrUpdateBanner: '/addOrUpdateBanner',
+    getAllBanner: '/getAllBanner',
+    deleteBannerById: '/deleteBanner'
 }
 
 const api = {
-    addBanner: imgUrl => request.get(url + base.addBanner, { params: { url: imgUrl } })
+    /**
+     * 添加或更新轮播图
+     * @param {Object}
+     * @returns 
+     */
+    addOrUpdateBanner: params => request.get(url + base.addOrUpdateBanner, { params }),
+    /**
+     * 
+     * @returns 所有轮播图
+     */
+    getAllBanner: () => request.get(url + base.getAllBanner),
+    /**
+     * 根据id删除轮播图数据
+     */
+    deleteBannerById: id => request.get(url + base.deleteBannerById, { params: { id } })
 }
 
 export default api
