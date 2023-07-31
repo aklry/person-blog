47a89e5cc289350044a462d87a4682758e4f4d8f
Aklry <74409739+aklry@users.noreply.github.com>
2023-07-31 17:14:45 +0800

    refactor: 删除无用的冗余代码

--- blog-ui/src/App.vue
+++ blog-ui/src/App.vue
@@ -1,24 +1,13 @@
 <template>
   <div id="app">
-    <router-view v-if="isRouterAlive" />
+    <router-view />
   </div>
 </template>
 <script>
 export default {
+  name: 'App',
   data() {
     return {
-      isRouterAlive: true
-    }
-  },
-  methods: {
-    reload() {
-      this.isRouterAlive = false
-      this.$nextTick(() => this.isRouterAlive = true)
-    }
-  },
-  provide() {
-    return {
-      reload: this.reload
     }
   }
 }
