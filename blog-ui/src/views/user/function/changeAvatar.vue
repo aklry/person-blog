433f7e6cb8e3fb29d2dc6f4e7a437507cfcbad45
Aklry <74409739+aklry@users.noreply.github.com>
2023-10-06 22:13:22 +0800

    feat: 实现element ui图片上传组件的自主上传功能

--- blog-ui/src/views/user/function/changeAvatar.vue
+++ blog-ui/src/views/user/function/changeAvatar.vue
@@ -2,14 +2,22 @@
     <div class="container">
         <el-button class="btn" type="primary" @click="dialogUploadVisible = true">上传头像</el-button>
         <el-dialog destroy-on-close :visible="dialogUploadVisible" center title="上传头像">
-            <el-upload class="avatar-uploader" :action="getActions" :show-file-list="false"
-                :on-success="handleAvatarSuccess" :before-upload="beforeAvatarUpload">
+            <el-upload
+                    class="avatar-uploader"
+                    :action="actions"
+                    :show-file-list="false"
+                    :on-success="handleAvatarSuccess"
+                    :before-upload="beforeAvatarUpload"
+                    :auto-upload="false"
+                    ref="upload"
+                    :on-change="handleAvatarChange"
+            >
                 <img v-if="imageUrl" :src="imageUrl" class="avatar">
                 <i v-else class="el-icon-plus avatar-uploader-icon"></i>
             </el-upload>
             <template #footer>
-                <el-button @click="dialogUploadVisible = false">取消</el-button>
-                <el-button type="primary" @click="dialogUploadVisible = false">确认</el-button>
+                <el-button @click="cancel">取消</el-button>
+                <el-button type="primary" @click="handleUpload">确认</el-button>
             </template>
         </el-dialog>
     </div>
@@ -27,14 +35,13 @@ export default {
     },
     methods: {
         handleAvatarSuccess(res, file) {
-            this.imageUrl = URL.createObjectURL(file.raw)
             localStorage.setItem('imageUrl', res)
             api.updateUserURL({
                 id: this.$store.state.userInfo.id,
                 url: res
             }).then(res => {
                 if (res.status === 200) {
-                    this.$message(res.data.message)
+                    this.$message.success(res.data.message)
                 }
             }).catch(error => console.log(error))
         },
@@ -49,11 +56,22 @@ export default {
                 this.$message.error('上传头像图片大小不能超过 2MB!');
             }
             return isJPG && isLt2M;
+        },
+        handleAvatarChange(file) {
+            this.imageUrl = URL.createObjectURL(file.raw)
+        },
+        handleUpload() {
+            this.dialogUploadVisible = false
+            this.$refs.upload.submit()
+        },
+        cancel() {
+            this.dialogUploadVisible = false
+            this.imageUrl = ''
         }
     },
     computed: {
-        getActions() {
-            return process.env.NODE_ENV === 'development' ? `${process.env.VUE_APP_SERVE}/uploadAvatar` : `${process.env.VUE_APP_SERVE}/blog/uploadAvatar`
+        actions() {
+            return `${process.env.VUE_APP_SERVE}/blog/uploadAvatar`
         }
     }
 }
